1aed8b4d977bada28f4371cdf6c759d9f87315ec
Volkov Dmitriy <vdy0555@gmail.com>
2023-10-15 20:26:04 +0700

    Prepare routes for all pages

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,8 +1,43 @@
 import { createBrowserRouter } from 'react-router-dom'
+import { routes } from '@/shared/const'
 import { Layout } from './Layout'
 
 export const router = createBrowserRouter([
   {
-    element: <Layout />
+    element: <Layout />,
+    children: [
+      {
+        path: routes.login(),
+        element: null
+      },
+      {
+        path: routes.registration(),
+        element: null
+      },
+      {
+        path: routes.profile(),
+        element: null
+      },
+      {
+        path: routes.item(),
+        element: null
+      },
+      {
+        path: routes.cart(),
+        element: null
+      },
+      {
+        path: routes.orders(),
+        element: null
+      },
+      {
+        path: routes.order(),
+        element: null
+      },
+      {
+        path: routes.purchase(),
+        element: null
+      }
+    ]
   }
 ])
--- src/shared/const/routes.ts
+++ src/shared/const/routes.ts
@@ -1,10 +1,11 @@
 export const routes = {
+  root: () => '/',
   login: () => '/login',
   registration: () => '/registration',
   profile: () => '/profile',
-  item: (itemId: string) => `/item/${itemId}`,
+  item: (itemId?: string) => `/item/${itemId || ':orderId'}`,
   cart: () => 'cart',
   orders: () => 'orders',
-  order: (orderId: string) => `/order/${orderId}`,
+  order: (orderId?: string) => `/order/${orderId || ':orderId'}`,
   purchase: () => 'purchase'
 }
