05fe21df7092b68fcfa156d1bd8b9b68977b3ba5
Volkov Dmitriy <vdy0555@gmail.com>
2023-10-16 20:41:56 +0700

    Add select uikit component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
-import { Button } from '@/shared/uikit'
+import React from 'react'
+import { Button, Select, type SelectOption } from '@/shared/uikit'
+
+const o = [
+  { label: 'one', value: 'one' },
+  { label: 'two', value: 'two' },
+  { label: 'Dthree', value: 'one' },
+  { label: 'Dthree', value: 'one' },
+  { label: 'Dthree', value: 'one' },
+  { label: 'Dthree', value: 'one' },
+  { label: 'Dthree', value: 'one' },
+  { label: 'Dthree', value: 'one' },
+  { label: 'Dthree', value: 'one' }
+]
 
 function App() {
+  const [input, setInput] = React.useState<SelectOption[]>([])
+
   return (
     <div className="wrapper">
       <Button alertType="danger" styleType="outlined">
         I am button
       </Button>
+      <Select
+        onChange={(newValue) => {
+          if (!!newValue) setInput(newValue)
+        }}
+        options={o}
+        value={input}
+        multiple={true}
+      ></Select>
     </div>
   )
 }
