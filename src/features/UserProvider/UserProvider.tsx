5178c7ac030f03ccdf7984ef9f8fad21f3d2c4c1
Volkov Dmitriy <vdy0555@gmail.com>
2023-10-16 21:15:48 +0700

    Add user context
    save user email, token, isAuth in local storage
    add user provider
    add local storage keys

---
+++ src/features/UserProvider/UserProvider.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { IS_AUTH_KEY, USER_INFO_KEY } from '@/shared/lib/const'
+import { UserContext, type UserContextInfo } from '@/shared/lib/contexts'
+import { useLocalStorage } from '@/shared/lib/hooks'
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const { value: isAuth, setValue: setIsAuth } = useLocalStorage(IS_AUTH_KEY, false)
+  const { value: user, setValue: setUser } = useLocalStorage<UserContextInfo>(USER_INFO_KEY, {
+    token: '',
+    email: ''
+  })
+
+  const login = () => {
+    setIsAuth(true)
+    // navigate(routes.main)
+  }
+
+  const logout = () => {
+    setIsAuth(false)
+    setUser({
+      token: '',
+      email: ''
+    })
+    // navigate(routes.main)
+  }
+
+  return <UserContext.Provider value={{ isAuth, user, login, logout }}>{children}</UserContext.Provider>
+}
