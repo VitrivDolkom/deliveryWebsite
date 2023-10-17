1ea902d8889ef8db06bde5a742d7b09713121304
Volkov Dmitriy <vdy0555@gmail.com>
2023-10-17 20:45:01 +0700

    Add location block
    select search
    add next address object if selected
    :

--- src/features/SelectLocation/SelectLocation.tsx
+++ src/features/SelectLocation/SelectLocation.tsx
@@ -1,26 +1,79 @@
-import { useFieldArray, useForm } from 'react-hook-form'
+import React from 'react'
+import Select, { SingleValue } from 'react-select'
 import { useRequest } from '@/shared/lib/hooks'
 
+const backendUrl = 'https://food-delivery.kreosoft.ru/api'
+
+interface SelectAddress {
+  address: SearchAddressModel
+  value: string
+  label: string
+}
+interface SelectObject {
+  options: SelectAddress[]
+  object: SelectAddress | null
+  title: string
+}
+
 export const SelectLocation = () => {
-  const { register, control, handleSubmit, formState, setValue } = useForm()
-  const { fields, append, remove } = useFieldArray({
-    control,
-    name: 'test'
-  })
+  const [addressObjects, setAddressObjects] = React.useState<SelectObject[]>([])
+  const { data: addressSearch, requestHandler: searchRequest } = useRequest<SearchAddressModel[]>(false)
 
-  const { data, error, isLoading } = useRequest<DishPagedListDto>(true, {
-    method: 'get',
-    url: 'https://food-delivery.kreosoft.ru/api/dish',
-    headers: {}
-  })
+  React.useEffect(() => {
+    searchRequest({
+      method: 'get',
+      url: `${backendUrl}/address/search`,
+      headers: {}
+    })
+  }, [])
 
-  // useEffect(() => {
-  //   requestHandler()
-  // }, [])
+  React.useEffect(() => {
+    if (!addressSearch || !addressSearch.length) return
 
-  if (isLoading) {
-    return <div>Loading</div>
+    setAddressObjects((prev) => [
+      ...prev,
+      {
+        object: null,
+        options: addressSearch.map((address) => ({ address, value: address.text, label: address.text })),
+        title: addressSearch[0].objectLevelText
+      }
+    ])
+  }, [addressSearch])
+
+  const onSelectChange = (newValue: SingleValue<SelectAddress>, index: number) => {
+    setAddressObjects((prev) => {
+      prev[index] = {
+        object: newValue,
+        options: prev[index].options,
+        title: prev[index].title
+      }
+
+      prev.splice(index + 1)
+
+      const url = `${backendUrl}/address/search?parentObjectId=${prev[index].object?.address.objectId}&query=`
+
+      searchRequest({
+        method: 'get',
+        url: url,
+        headers: {}
+      })
+
+      return [...prev]
+    })
   }
 
-  return <div></div>
+  return (
+    <div>
+      {addressObjects?.map((select, index) => (
+        <div key={index}>
+          <span>{select.title}</span>
+          <Select
+            value={addressObjects[index].object}
+            options={select.options}
+            onChange={(newValue: SingleValue<SelectAddress>) => onSelectChange(newValue, index)}
+          />
+        </div>
+      ))}
+    </div>
+  )
 }
