b46f5ac4b6f11683a56800448468b7c5e446322f
Volkov Dmitriy <vdy0555@gmail.com>
2023-10-20 08:27:24 +0700

    Fix names request -> config

--- src/shared/api/requests/account/putProfileConfig.ts
+++ src/shared/api/requests/account/putProfileConfig.ts
@@ -1,7 +1,7 @@
 import { authHeaders } from '../../authHeaders'
 import { request } from '../../request'
 
-export const getProfileRequest = (dto: Partial<UserDto>, tokenResponse: TokenResponse) =>
+export const putProfileConfig = (dto: Partial<UserDto>, tokenResponse: TokenResponse) =>
   request<Partial<UserDto>>({
     config: { method: 'put', data: dto, headers: { ...authHeaders(tokenResponse) } },
     url: '/account/profile'
