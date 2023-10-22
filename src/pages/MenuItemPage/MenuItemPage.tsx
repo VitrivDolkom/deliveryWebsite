f8198d4c1910470fd34fa34f3889293fb758c8e0
Volkov Dmitriy <vdy0555@gmail.com>
2023-10-22 10:37:30 +0700

    Add base menu item page structure

--- src/pages/MenuItemPage/MenuItemPage.tsx
+++ src/pages/MenuItemPage/MenuItemPage.tsx
@@ -1,1 +1,38 @@
-export const MenuItemPage = () => <div>MenuItemPage</div>
+import { DishRating } from '@/features'
+import { MenuItemImage } from '@/shared/components'
+import { Typography } from '@/shared/uikit'
+import s from './styles.module.css'
+
+const dish: DishDto = {
+  name: '4 сыра',
+  description: '4 сыра: «Моцарелла», «Гауда», «Фета», «Дор-блю», сливочно-сырный соус, пряные травы',
+  price: 360,
+  image: 'https://mistertako.ru/uploads/products/77888c7e-8327-11ec-8575-0050569dbef0.',
+  vegetarian: true,
+  rating: 8.75,
+  category: 'Pizza',
+  id: 'e4698ac7-7d9e-456e-2741-08dbc899a338'
+}
+
+export const MenuItemPage = () => (
+  <div className={s.wrapper}>
+    <div className={s.img}>
+      <MenuItemImage image={dish.image} vegetarian={dish.vegetarian} />
+    </div>
+    <div className={s.top}>
+      <Typography tag="h2" variant="t3" className={s.name}>
+        {dish.name}
+      </Typography>
+      <Typography tag="p" variant="t1">
+        Категория блюда - {dish.category}
+      </Typography>
+    </div>
+    <DishRating rating={dish.rating} />
+    <Typography tag="p" variant="t1" className={s.description}>
+      {dish.description}
+    </Typography>
+    <Typography tag="p" variant="t1">
+      Цена - {dish.price}р/шт
+    </Typography>
+  </div>
+)
